7318fea335ecfcd3e3f489eb6fcdae76a852c536
Max Rudolph <maxrudolph@ucdavis.edu>
2023-01-16 23:03:53 -0800

    add code to get temperature at basic nodes - helpful for paraview

--- TemperatureCylindrical.jl
+++ TemperatureCylindrical.jl
@@ -194,3 +194,16 @@ function subgrid_temperature_relaxation_center!(markers::Markers,grid::Cartesian
     return dTm
     
 end
+
+function temperature_to_basic_nodes(grid::CartesianGrid,Tc::Matrix{Float64})
+     if size(Tc,1) != grid.ny+1 || size(Tc,2) != grid.nx + 1
+        error("temperature array needs to contain ghost values")
+    end
+    Tn = zeros(grid.ny,grid.nx)
+    for i in 1:ny
+        for j in 1:nx
+            Tn[i,j] = 0.25*(Tc[i,j]+Tc[i,j+1]+Tc[i+1,j]+Tc[i+1,j+1])
+        end
+    end
+    return Tc
+end
