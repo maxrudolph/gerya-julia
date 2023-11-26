f60cea858d852a15abaf4259891b1005530cc8f9
Christian Aguirre <visdenchris@gmail.com>
2023-11-26 12:31:45 -0800

    Setting up criteria for convergence

--- TemperatureEntropy.jl
+++ TemperatureEntropy.jl
@@ -21,7 +21,6 @@ function entropy_of_P_T(X::Float64,T::Float64,options::Dict)
         T - temperature 
         options - allow the simulation of optional keyword arguments from a dictonary
     """
-    
     Hfus = options["latent heat of fusion"] # J/kg
     Cv = options["specific heat"] # J/kg*K
     Tm = options["Tm"] # K 
@@ -44,7 +43,6 @@ function get_lambda1(options::Dict)
     Arguments:
         options - allow the simulation of optional keyword arguments from a dictonary
     """
-    
     L = options["latent heat of fusion"] # J/kg
     c = options["specific heat"] # J/kg*K
     dT = options["Tm"] - options["To"] # K 
@@ -65,7 +63,6 @@ function get_t(lambda1::Float64,options::Dict)
         lambda1 - 
         options - allow the simulation of optional keyword arguments from a dictonary
     """
-    
     kappa = options["thermal diffusivity"] # m^2/s
     ym = options["ym"]^2 # m 
     lambda = lambda1^2
@@ -80,7 +77,6 @@ function get_theta(y::Float64,t::Float64,lambda1::Float64)
         t - time in seconds
         lambda1 - 
     """
-    
     kappa = options["thermal diffusivity"] # m^2/s
     eta = y/(2*sqrt(kappa*t))
     theta = erf(eta)/erf(lambda1)
@@ -93,7 +89,6 @@ function stefan_initial_condition(theta::Float64,options::Dict)
         theta - 
         options - allow the simulation of optional keyword arguments from a dictonary
     """
-    
     Tm = options["Tm"] # K
     To = options["To"] # K
     dT = Tm-To
@@ -101,41 +96,31 @@ function stefan_initial_condition(theta::Float64,options::Dict)
     return T
 end
 
-function compute_q_cond(grid::CartesianGrid,T::Matrix{Float64},kThermal::Matrix{Float64},stringtype::String) 
-    if stringtype == "vx"
-        q_cond = zeros(grid.ny+1,grid.nx)
-        for j in 1:grid.nx
-            for i in 1:grid.ny
-                q_cond[i,j] = kThermal[i,j] * (T[i,j+1]-T[i,j])/(grid.xc[j+1]-grid.xc[j])
-                # if j == 1
-                #     q_cond[i,j] = kThermal[i,j]*(T[i,j]/grid.xc[j])
-                # else
-                #     q_cond[i,j] = kThermal[i,j]*((T[i,j]-T[i,j-1])/(grid.xc[j]-grid.xc[j-1]))
-                # end
-            end
-        end
-    elseif stringtype == "vy"
-        q_cond = zeros(grid.ny,grid.nx+1)
-        for j in 1:grid.nx
-            for i in 1:grid.ny
-                q_cond[i,j] = kThermal[i,j] * (T[i+1,j]-T[i,j])/(grid.yc[i+1]-grid.yc[i])
-                # if i == 1
-                #     q_cond[i,j] = kThermal[i,j]*(T[i,j]/grid.yc[i])
-                # else
-                #     q_cond[i,j] = kThermal_[i,j]*((T[i,j]-T[i-1,j])/(grid.yc[i]-grid.yc[i-1]))
-                # end
-            end
+function compute_q_cond(grid::CartesianGrid,T::Matrix{Float64},k_vx::Matrix{Float64},k_vy::Matrix{Float64}) 
+    # Note - this function expects T to include ghost values on all sides of the domain.
+    q_vx = zeros(grid.ny,grid.nx)
+    q_vy = zeros(grid.ny,grid.nx)
+    for j in 1:grid.nx
+        for i in 1:grid.ny
+            q_vx[i,j] = -k_vx[i,j] * (T[i,j+1]-T[i,j])/(grid.xc[j+1]-grid.xc[j])
         end
     end
-    return q_cond  
+
+    for j in 1:grid.nx
+        for i in 1:grid.ny
+            q_vy[i,j] = -k_vy[i,j] * (T[i+1,j]-T[i,j])/(grid.yc[i+1]-grid.yc[i])            
+        end
+    end
+
+    return q_vx,q_vy
 end
 
 function compute_S_new(grid::CartesianGrid,Tlast::Matrix{Float64},rho::Matrix{Float64},H::Matrix{Float64},qx::Matrix{Float64},qy::Matrix{Float64},S_old::Matrix{Float64},dt::Float64)
     S = zeros(grid.ny,grid.nx)
-    for j in 1:grid.nx
-        for i in 1:grid.ny
-            S[i,j] = begin (dt/(rho[i,j]*Tlast[i,j])) * (-((qx[i+1,j]-qx[i,j])/(grid.xc[j+1]-grid.xc[j]) 
-                        + (qy[i,j+1]-qy[i,j])/(grid.yc[i+1]-grid.yc[i])) + H[i,j]) + S_old[i,j] end
+    for j in 2:grid.nx
+        for i in 2:grid.ny
+            S[i,j] = begin (dt/(rho[i,j]*Tlast[i,j])) * (-((qx[i,j]-qx[i,j-1])/(grid.x[j]-grid.x[j-1]) 
+                        + (qy[i,j]-qy[i-1,j])/(grid.y[i]-grid.y[i-1])) + H[i,j]) + S_old[i,j] end
         end
     end
     return S
@@ -150,3 +135,22 @@ function update_temp_from_entropy(grid::CartesianGrid,Xlast::Matrix{Float64},S_o
     end
     return T
 end
+
+function update_melt_fraction(grid::CartesianGrid,T_new::Matrix{Float64},S_new::Matrix{Float64},options::Dict)
+    X = zeros(grid.ny,grid.nx)
+    for j in 2:grid.nx
+        for i in 2:grid.ny
+            X[i,j] = compute_melt_fraction(T_new[i,j],S_new[i,j],options)
+        end
+    end
+    return X
+end
+# function update_temp_from_entropy(grid::CartesianGrid,Xlast::Matrix{Float64},S_old::Matrix{Float64},options::Dict)
+#     T = zeros(grid.ny+1,grid.nx+1)
+#     for j in 2:grid.nx
+#         for i in 2:grid.ny
+#             T[i,j] = temp_of_P_S(Xlast[i,j],S_old[i,j],options)
+#         end
+#     end
+#     return T
+# end
