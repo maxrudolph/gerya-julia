cf5224b427a82a2c13341cf0e28378cd90410b22
Christian Aguirre <visdenchris@gmail.com>
2023-12-08 08:54:37 -0800

    Testing Stefan Initial Conditions

--- TemperatureEntropy.jl
+++ TemperatureEntropy.jl
@@ -1,42 +1,5 @@
-function temp_of_P_S(X::Float64,S::Float64,options::Dict)
-    """
-    Arguments:
-        X - melt fraction
-        S - entropy 
-        options - allow the simulation of optional keyword arguments from a dictonary
-    """  
-    Hfus = options["latent heat of fusion"] 
-    Cv = options["specific heat"] 
-    Tm = options["Tm"]
-    Tref = options["Tref"]
-    T = exp((S/Cv) + log(Tref) - (Hfus*X)/(Cv*Tm))
-    return T
-end
-
-function entropy_of_P_T(X::Float64,T::Float64,options::Dict)
-    """
-    Arguments:
-        X - melt fraction
-        T - temperature 
-        options - allow the simulation of optional keyword arguments from a dictonary
-    """
-    Hfus = options["latent heat of fusion"] # J/kg
-    Cv = options["specific heat"] # J/kg*K
-    Tm = options["Tm"] # K 
-    Tref = options["Tref"] # K 
-    S = Cv*(log(T) - log(Tref)) + (Hfus*X)/Tm
-    return S
-end
-
-function compute_melt_fraction(T::Float64,S::Float64,options::Dict)
-    Hfus = options["latent heat of fusion"] # J/kg
-    Cv = options["specific heat"] # J/kg*K
-    Tm = options["Tm"] # K 
-    Tref = options["Tref"] # K 
-    X = -Cv*(log(T) - log(Tref))*(Tm/Hfus) + (S*Tm)/Hfus
-    return X
-end 
-
+#### begin #### 
+##### Equations for setting up Stefan conidtion #####
 function get_lambda1(options::Dict)
     """
     Arguments:
@@ -54,8 +17,11 @@ end
 function get_t(lambda1::Float64,options::Dict)
     """
     Arguments:
-        lambda1 - 
+        lambda1 - constant
         options - allow the simulation of optional keyword arguments from a dictonary
+
+    Returns: 
+        t - time in units of (seconds)
     """
     kappa = options["thermal diffusivity"] # m^2/s
     ym = options["ym"]^2 # m 
@@ -64,7 +30,7 @@ function get_t(lambda1::Float64,options::Dict)
     return t
 end
 
-function get_y(lambda::Float64,t::Float64,options::Dict)
+function get_y(lambda1::Float64,t::Float64,options::Dict)
     kappa = options["thermal diffusivity"] # m^2/s
     y = 2*lambda1*sqrt(kappa*t)
     return y
@@ -73,9 +39,9 @@ end
 function get_theta(y::Float64,t::Float64,lambda1::Float64)
     """
     Arguments:
-        y - depth position in meters
-        t - time in seconds
-        lambda1 - 
+        y - depth position in units of (meters)
+        t - time in units of (seconds)
+        lambda1 - constant
     """
     kappa = options["thermal diffusivity"] # m^2/s
     eta = y/(2*sqrt(kappa*t))
@@ -86,8 +52,11 @@ end
 function stefan_initial_condition(theta::Float64,options::Dict)
     """
     Arguments:
-        theta - 
+        theta - constant
         options - allow the simulation of optional keyword arguments from a dictonary
+    
+    Returns:
+        T - temperature in units of (Kelvin)
     """
     Tm = options["Tm"] # K
     To = options["To"] # K
@@ -95,7 +64,60 @@ function stefan_initial_condition(theta::Float64,options::Dict)
     T = (theta*dT) + To
     return T
 end
+#### end ####
 
+#### begin ####
+#### Equations for T,X = fcn(S) and S = fcn(T,X) #####
+function compute_T_X_from_S(S::Float64,options::Dict)
+    """
+    Arguments:
+        S - entropy in untis of (J/kg*K)
+        options - allow the simulation of optional keyword arguments from a dictonary
+    
+    Returns:
+        T - temperature in units of (Kelvin)
+    """
+    Hfus = options["latent heat of fusion"] # J/kg
+    Cv = options["specific heat"] # J/kg*K
+    Tm = options["Tm"] # K 
+    if S < 0 
+        T = exp((S/Cv))*Tm
+        X = 0.0
+    elseif S > (Hfus/Tm)
+        T = exp(((S*Tm)-Hfus)/(Cv*Tm))*Tm
+        X = 1.0
+    else 
+        X = (S*Tm)/Hfus
+        T = Tm
+    end
+    return T,X
+end
+
+function compute_S_from_T_X(X::Float64,T::Float64,options::Dict)
+    """
+    Arguments:
+        X - melt fraction (unitless)
+        T - temperature in units of (Kelvin)
+        options - allow the simulation of optional keyword arguments from a dictonary
+    Returns:
+        S - entropy in untis of (J/kg*K)
+    """
+    Hfus = options["latent heat of fusion"] # J/kg
+    Cv = options["specific heat"] # J/kg*K
+    Tm = options["Tm"] # K 
+    if T < Tm 
+        S = Cv*(log(T)-log(Tm))
+    elseif T > Tm 
+        S = Cv*(log(T)-log(Tm)) + (Hfus/Tm)
+    else 
+        S = (Hfus/Tm)*X
+    end
+    return S
+end
+#### end ####
+
+#### begin #### 
+#### Numerical equations for conductive heat flux, entropy ####
 function compute_q_cond(grid::CartesianGrid,T::Matrix{Float64},k_vx::Matrix{Float64},k_vy::Matrix{Float64}) 
     # Note - this function expects T to include ghost values on all sides of the domain.
     q_vx = zeros(grid.ny,grid.nx)
@@ -115,13 +137,6 @@ end
 
 function compute_S_new(grid::CartesianGrid,Tlast::Matrix{Float64},rho::Matrix{Float64},H::Matrix{Float64},qx::Matrix{Float64},qy::Matrix{Float64},S_old::Matrix{Float64},dt::Float64)
     S = zeros(grid.ny+1,grid.nx+1)
-    # for j in 2:grid.nx
-    #     for i in 2:grid.ny
-    #         S[i,j] = begin (dt/(rho[i,j]*Tlast[i,j])) * (-((qx[i,j]-qx[i,j-1])/(grid.x[j]-grid.x[j-1]) 
-    #                     + (qy[i,j]-qy[i-1,j])/(grid.y[i]-grid.y[i-1])) + H[i,j]) + S_old[i,j] end
-    #     end
-    # end
-    # return S
     for j in 2:grid.nx
         for i in 2:grid.ny
             S[i,j] = (dt/( rho[i,j] * Tlast[i,j]) ) * 
@@ -133,45 +148,101 @@ function compute_S_new(grid::CartesianGrid,Tlast::Matrix{Float64},rho::Matrix{Fl
     end
     return S
 end
+#### end #### 
 
-function update_temp_from_entropy(grid::CartesianGrid,X::Matrix{Float64},S::Matrix{Float64},options::Dict)
-    T = zeros(grid.ny,grid.nx)
-    for j in 2:grid.nx
-        for i in 2:grid.ny
-            T[i,j] = temp_of_P_S(X[i,j],S[i,j],options)
-        end
-    end
-    return T
-end
-
-function update_melt_fraction(grid::CartesianGrid,T::Matrix{Float64},S::Matrix{Float64},options::Dict)
-    X = zeros(grid.ny,grid.nx)
-    for j in 2:grid.nx
-        for i in 2:grid.ny
-            X[i,j] = compute_melt_fraction(T[i,j],S[i,j],options)
-        end
-    end
-    return X
-end
-
-function update_TX_from_S(S::Float64,options::Dict)
-    Hfus = options["latent heat of fusion"] # J/kg
-    Cv = options["specific heat"] # J/kg*K
-    # Tref = options["Tref"] # K
-    Tm = options["Tm"] # K 
-    # X = zeros(51,50)
-    # T = zeros(51,50)
-    if S < 0 
-        T = exp((S/Cv))*Tm
-        X = 0.0
-    elseif S > (Hfus/Tm)
-        T = exp(((S*Tm)-Hfus)/(Cv*Tm))*Tm
-        X = 1.0
-    else 
-        X = (S*Tm)/Hfus
-        T = Tm
-    end
+#### begin ###
+#### Update functions ####
+function update_T_X_from_S(Snew::Matrix{Float64},options::Dict)
+    """
+    Arguments:
+        S - a matrix of entropy in untis of (J/kg*K)
+        options - allow the simulation of optional keyword arguments from a dictonary
+    
+    Returns:
+        X - a martix of melt fraction (unitless)
+        T - a matrix of temperature in units of (Kelvin)
+    """
+    # Broadcasting the function to each element in S
+    results = compute_T_X_from_S.(Snew, Ref(options))
+    # Extracting T and X matrices from the results
+    T = [result[1] for result in results]
+    X = [result[2] for result in results]
     return T,X
 end
+#### end ####
 
-# function update_markers_TX_from_S()
+
+#### begin ####
+#### Function to compute the ghost nodes ####
+function ghost_temperature_center_TXS(grid::CartesianGrid,T::Matrix{Float64},S::Matrix{Float64},X::Matrix{Float64},bctype,bcval,options::Dict)
+    # along the left, right, top, and bottom (in that order)
+    # -1 = insulating, 1 = constant temp
+
+    Tbctype = [-1,-1,1,1] #left, right, top, bottom
+    
+    # Extracting the boundary condition types for the left, right, top, and bottom
+    bcleft  = bctype[1] 
+    bcright = bctype[2]
+    bctop   = bctype[3]
+    bcbottom  = bctype[4]
+
+    # Creating a matrix then copying the values from the original matrix into the interior of new matrix
+    Tpad = Array{Float64,2}(undef,grid.ny+1,grid.nx+1)   
+    Tpad[1:grid.ny,1:grid.nx] = T[1:grid.ny,1:grid.nx]
+    Spad = Array{Float64,2}(undef,grid.ny+1,grid.nx+1)   
+    Spad[1:grid.ny,1:grid.nx] = S[1:grid.ny,1:grid.nx]
+    Xpad = Array{Float64,2}(undef,grid.ny+1,grid.nx+1)   
+    Xpad[1:grid.ny,1:grid.nx] = X[1:grid.ny,1:grid.nx]
+
+    # Applying the boundary condition along top of the domain
+    # -1 = insulating, 1 = constant temp
+    if bctop == 1
+        Tpad[1,2:grid.nx] = 2.0*bcval[3] .- Tpad[2,2:grid.nx]
+        Spad[1,2:grid.nx] = compute_S_from_T_X.(Xpad[2,2:grid.nx],Tpad[2,2:grid.nx],Ref(options))
+        Xpad[1,2:grid.nx] = Xpad[2,2:grid.nx]
+    elseif bctop == -1
+	    Tpad[1,2:grid.nx] = Tpad[2,2:grid.nx] .- ((grid.yc[2]-grid.yc[1]) * bcval[3])
+        Spad[1,2:grid.nx] = compute_S_from_T_X.(Xpad[2,2:grid.nx],Tpad[2,2:grid.nx],Ref(options))
+        Xpad[1,2:grid.nx] = Xpad[2,2:grid.nx]
+    end
+
+    # Applying the boundary condition along bottom of the domain
+    # -1 = insulating, 1 = constant temp
+    if bcbottom == 1
+        Tpad[grid.ny+1,2:grid.nx] = 2.0*bcval[4] .- Tpad[grid.ny,2:grid.nx]
+        Spad[grid.ny+1,2:grid.nx] = compute_S_from_T_X.(Xpad[grid.ny,2:grid.nx],Tpad[grid.ny,2:grid.nx],Ref(options))
+        Xpad[grid.ny+1,2:grid.nx] = Xpad[grid.ny,2:grid.nx] 
+    elseif bcbottom == -1
+        Xpad[grid.ny+1,2:grid.nx] = Xpad[grid.ny,2:grid.nx]
+        Tpad[grid.ny+1,2:grid.nx] = Tpad[grid.ny,2:grid.nx] .+ ((grid.yc[grid.ny+1]-grid.yc[grid.ny]) * bcval[4])
+        Spad[grid.ny+1,2:grid.nx] = compute_S_from_T_X.(Xpad[grid.ny,2:grid.nx],Tpad[grid.ny,2:grid.nx],Ref(options))
+    end
+
+    # Applying the boundary condition along left of the domain
+    # -1 = insulating, 1 = constant temp
+    if bcleft == -1
+        Tpad[:,1] = Tpad[:,2] # insulating
+        Spad[:,1] = compute_S_from_T_X.(Xpad[:,2],Tpad[:,2],Ref(options))
+        Xpad[:,1] = Xpad[:,2]
+    elseif bcleft == 1
+        println("assigning left boundary temperature ",bcval[1])
+        Tpad[:,1] = 2.0*bcval[1] .- Tpad[:,2]
+        Spad[:,1] = compute_S_from_T_X.(Xpad[:,2],Tpad[:,2],Ref(options))
+        Xpad[:,1] = Xpad[:,2]
+    end
+
+    # Applying the boundary condition along right of the domain
+    # -1 = insulating, 1 = constant temp
+    if bcright == -1
+        Tpad[:,grid.nx+1] = Tpad[:,grid.nx] # insulating
+        Spad[:,grid.nx+1] = compute_S_from_T_X.(Xpad[:,grid.nx],Tpad[:,grid.nx],Ref(options))
+        Xpad[:,grid.nx+1] = Xpad[:,grid.nx]
+    elseif bcright == 1
+        Tpad[:,grid.nx+1] = 2.0*bcval[2] .- Tpad[:,grid.nx]
+        Spad[:,grid.nx+1] = compute_S_from_T_X.(Xpad[:,grid.nx],Tpad[:,grid.nx],Ref(options))
+        Xpad[:,grid.nx+1] = Xpad[:,grid.nx]
+    end    
+
+    return Tpad,Spad,Xpad
+end
+#### end ####
