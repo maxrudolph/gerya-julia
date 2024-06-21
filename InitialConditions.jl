18ecd793b26f563da4811586387d216b7a1c8b15
Christian Aguirre <visdenchris@gmail.com>
2024-06-20 21:04:15 -0700

    added julia file that contains the initial setup/conditions for model

---
+++ InitialConditions.jl
@@ -0,0 +1,54 @@
+### Initial Model Setup ###
+## starts here ##
+struct Materials
+    alpha::Vector{Float64} # Thermal expansion (1/K)
+    rho0::Vector{Float64} # Density (kg/m^3)
+    Hr::Vector{Float64} # Radiogenic heat production (W/m^3)
+    Cp::Vector{Float64} # Heat capacity (J/kg*K)
+    kThermal::Vector{Float64} # Thermal conductivity (W/m*K)
+    function Materials()
+        new([0.0],[1e3],[0.0],[2.1e3],[2.2])
+    end    
+end
+
+function initial_conditions!(markers::Markers,materials::Materials,options::Dict)
+    material = markers.integerFields["material"]
+    T = markers.scalarFields["T"]
+    S = markers.scalarFields["S"]
+    X = markers.scalarFields["X"]
+    alpha = markers.scalarFields["alpha"]
+    Cp = markers.scalarFields["Cp"]
+    Hr = markers.scalarFields["Hr"]
+    kThermal = markers.scalarFields["kThermal"]
+    for i in 1:markers.nmark
+        mx = markers.x[1,i]
+        my = markers.x[2,i]
+        hice = initial_ice_depth(mx,options)
+        if my > hice # subsurface global ocean
+            markers.integers[material,i] = 1
+            markers.scalars[alpha,i] = materials.alpha[1]
+            markers.scalars[Cp,i] = materials.Cp[1]
+            markers.scalars[Hr,i] = materials.Hr[1]
+            markers.scalars[kThermal,i] = materials.kThermal[1]
+            markers.scalars[T,i] = 273.0
+            markers.scalars[X,i] = 1.0
+            markers.scalars[S,i] = compute_S_from_T_X(markers.scalars[X,i],markers.scalars[T,i],options)
+        elseif my < hice # icy shell
+            markers.integers[material,i] = 1
+            markers.scalars[alpha,i] = materials.alpha[1]
+            markers.scalars[Cp,i] = materials.Cp[1]
+            markers.scalars[Hr,i] = materials.Hr[1]
+            markers.scalars[kThermal,i] = materials.kThermal[1]
+            markers.scalars[T,i] = 100.0+((273.0-100.0)/hice)*(my)
+            markers.scalars[X,i] = 0.0
+            markers.scalars[S,i] = compute_S_from_T_X(markers.scalars[X,i],markers.scalars[T,i],options)           
+        end
+    end
+    # end loop over markers
+    update_marker_prop!(markers,materials)
+end
+
+function initial_ice_depth(x::Float64,options::Dict)
+    return options["ice thickness"] + options["amplitude"]*cos( (pi*(x-options["wavelength"]))/options["wavelength"] )
+end
+## ends here ##
